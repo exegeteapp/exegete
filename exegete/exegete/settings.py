4ec47ec69084f039d698741264ce0c73f07e7dd8
Grahame Bowland <grahame@oreamnos.com.au>
2021-05-30 22:22:46 +0800

    lint

--- exegete/exegete/settings.py
+++ exegete/exegete/settings.py
@@ -162,4 +162,4 @@ if not DEBUG:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     # caddy or nginx sitting in front of the app
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
