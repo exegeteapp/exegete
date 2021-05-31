667b134820654ad30195f0f3f6f40cf318327083
Grahame Bowland <grahame@oreamnos.com.au>
2021-06-01 00:20:56 +0800

    react-router; fontawesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { HashRouter, Route, useHistory } from 'react-router-dom';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+function Header() {
+    return (
+        <Navbar className="d-print-none" fixed="top" color="dark" dark expand="md">
+            <Container>
+                <NavbarBrand tag={Link} to="/"><FontAwesomeIcon color="purple" icon={faBook} /> exegete.app</NavbarBrand>
+            </Container>
+        </Navbar>
+    );
 }
+
+function Exegete({ match }: { match: any }) {
+    return <>
+        <Header />
+        <Container id="main">
+            <p></p>
+        </Container>
+    </>;
+}
+
+function App() {
+    return (
+        <HashRouter>
+            <Route path="/:slug?" render={(props) => <Exegete match={props.match} />} />
+        </HashRouter>
+    );
+}
+
 
 export default App;
