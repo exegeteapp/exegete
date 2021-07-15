13319a90617c29ae89931d2866f9b594702ea17a
Grahame Bowland <grahame@oreamnos.com.au>
2021-07-16 00:17:08 +0800

    move to yarn 2

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,25 @@
-const proxy = require("http-proxy-middleware")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(proxy("/api/", { target: "http://localhost:8000/" }))
-    app.use(proxy("/admin/", { target: "http://localhost:8000/" }))
-    app.use(proxy("/django-static/", { target: "http://localhost:8000/" }))
-}
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/admin',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/django-static',
+    createProxyMiddleware({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  );
+};
