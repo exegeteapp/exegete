cf8cf0ff28683039f9edf1001485fb4f2e3260e3
Grahame Bowland <grahame@oreamnos.com.au>
2021-11-02 17:20:45 +0800

    feedback from Robert on front page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -7,12 +7,17 @@ function Home(props: RouteComponentProps) {
         <Container id="main">
             <h1 className="display-3">Welcome!</h1>
             <Row>
-                <Col md={{ size: 5, offset: 0 }}>
+                <Col md={{ size: 7, offset: 0 }}>
                     <p className="lead">
                         exegete.app is an online environment for biblical exegesis, currently under active development.
                     </p>
+                    <div className="d-grid gap-2">
+                        <Button color="success btn-lg">Try exegete.app now!</Button>
+                        <p className="text-center">No signup needed.</p>
+                    </div>
                 </Col>
-                <Col md={{ size: 5, offset: 1 }}>
+                <Col md={{ size: 3, offset: 1 }}>
+                    <p className="text-center">Already have an account? Sign in.</p>
                     <Form inline>
                         <FormGroup>
                             <Label
@@ -46,7 +51,7 @@ function Home(props: RouteComponentProps) {
                         <div className="d-grid gap-2">
                             <Button color="primary">Log in</Button>
                             <p className="text-center">Forgotten password?</p>
-                            <hr style={{margin: "0 0 1rem 0"}} />
+                            <hr style={{ margin: "0 0 1rem 0" }} />
                             <Button color="success" tag={Link} to="/register">Create New Account</Button>
                         </div>
                     </Form>
