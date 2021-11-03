643eb493602236cc50f7b534258e3b596d71203f
Grahame Bowland <grahame@oreamnos.com.au>
2021-11-04 00:24:02 +0800

    add table for workspace storage

--- backend/exegete/api/database.py
+++ backend/exegete/api/database.py
@@ -1,7 +1,9 @@
 import databases
-import sqlalchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.dialects.postgresql import JSONB
 from exegete.settings import settings
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy import GUID
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy import Column, Text
 
@@ -14,6 +16,18 @@ Base: DeclarativeMeta = declarative_base()
 class UserTable(Base, SQLAlchemyBaseUserTable):
     name = Column(Text, default=False, nullable=False)
     affiliation = Column(Text, default=False, nullable=False)
+
+
+class Workspace(Base):
+    __tablename__ = "workspace"
+    id = Column(GUID, primary_key=True)
+    owner_id = Column(
+        ForeignKey(UserTable.id),
+        nullable=False,
+        primary_key=True,
+    )
+    title = Column(Text, default=False, nullable=False)
+    workspace = Column(JSONB, default=False, nullable=False)
 
 
 def get_user_db():
