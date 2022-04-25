7dcff5774024036c503bf2061502ccfc409b6bee
Grahame Bowland <grahame@oreamnos.com.au>
2022-04-25 22:46:30 +0800

    cache scripture requests; fairly major UX speedup

--- frontend/src/scripture/ScriptureAPI.tsx
+++ frontend/src/scripture/ScriptureAPI.tsx
@@ -28,10 +28,24 @@ export interface ScriptureParams extends ScriptureBookChapter {
     shortcode: string;
 }
 
+const CACHE_SIZE = 10;
+let scriptureCache: [string, ScriptureObject[]][] = [];
+
+const cache_key = (params: ScriptureParams): string => {
+    return `${params.shortcode}_${params.book}_${params.chapter_start}_${params.verse_start}_${params.chapter_end}_${params.verse_end}`;
+};
+
 export const getScripture = async (params: ScriptureParams): Promise<ScriptureObject[] | null> => {
+    const key = cache_key(params);
+    const match = scriptureCache.find(([k]) => key === k);
+    if (match) {
+        return match[1];
+    }
     try {
         const url = `/api/v1/scripture/verses/${params.shortcode}/${params.book}`;
         const resp = await axios.get<ScriptureObject[]>(url, { params: params });
+        scriptureCache.push([key, resp.data]);
+        scriptureCache = scriptureCache.slice(scriptureCache.length - CACHE_SIZE, scriptureCache.length);
         return resp.data;
     } catch (error: any) {
         return null;
