bb1a6502005d18675c395194f9fc20fa962e4592
Grahame Bowland <grahame@oreamnos.com.au>
2022-11-18 19:04:49 +0800

    use rtk-query to get news (#50)

--- frontend/src/components/News.tsx
+++ frontend/src/components/News.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { useGetNewsQuery } from "../api/mdcontent";
 
 export const News: React.FC<React.PropsWithChildren<unknown>> = () => {
-    const [news, setNews] = React.useState<string>("");
-
-    React.useEffect(() => {
-        fetch("/news.md")
-            .then((response) => response.text())
-            .then((text) => setNews(text));
-    }, []);
+    const { data: news } = useGetNewsQuery();
 
     return (
         <>
             <h1 className="display-5">News</h1>
-            <ReactMarkdown children={news} />
+            <ReactMarkdown children={news || ""} />
         </>
     );
 };
--- frontend/src/exegete/store.tsx
+++ frontend/src/exegete/store.tsx
@@ -4,16 +4,21 @@ import { backendApi } from "../api/api";
 import workspaceReducer from "../workspace/Workspace";
 import userReducer from "../user/User";
 import { backendAuthApi } from "../api/apiauth";
+import { mdcontentApi } from "../api/mdcontent";
 
 export const store = configureStore({
     reducer: {
         [backendApi.reducerPath]: backendApi.reducer,
         [backendAuthApi.reducerPath]: backendAuthApi.reducer,
+        [mdcontentApi.reducerPath]: mdcontentApi.reducer,
         workspace: workspaceReducer,
         user: userReducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(backendApi.middleware).concat(backendAuthApi.middleware),
+        getDefaultMiddleware()
+            .concat(backendApi.middleware)
+            .concat(backendAuthApi.middleware)
+            .concat(mdcontentApi.middleware),
 });
 
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
