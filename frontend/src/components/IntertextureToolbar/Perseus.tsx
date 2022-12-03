8dd8187eeedd634b92c0205530653d2b1f37e91a
Grahame Bowland <grahame@oreamnos.com.au>
2022-12-03 22:29:34 +0800

    perseus: use https links

--- frontend/src/components/IntertextureToolbar/Perseus.tsx
+++ frontend/src/components/IntertextureToolbar/Perseus.tsx
@@ -1,9 +1,9 @@
 export const GreekWordStudy = (word: string) => {
-    return `http://www.perseus.tufts.edu/hopper/morph?l=${encodeURIComponent(word)}&la=greek`;
+    return `https://www.perseus.tufts.edu/hopper/morph?l=${encodeURIComponent(word)}&la=greek`;
 };
 
 export const EnglishWordSearch = (word: string) => {
-    return `http://www.perseus.tufts.edu/hopper/searchresults?target=en&collections=Perseus%3Acollection%3AGreco-Roman&collections=Perseus%3Acollection%3AArabic&collections=Perseus%3Acollection%3AGermanic&collections=Perseus%3Acollection%3Acwar&collections=Perseus%3Acollection%3ARenaissance&collections=Perseus%3Acollection%3ARichTimes&collections=Perseus%3Acollection%3APDILL&all_words=${encodeURIComponent(
+    return `https://www.perseus.tufts.edu/hopper/searchresults?target=en&collections=Perseus%3Acollection%3AGreco-Roman&collections=Perseus%3Acollection%3AArabic&collections=Perseus%3Acollection%3AGermanic&collections=Perseus%3Acollection%3Acwar&collections=Perseus%3Acollection%3ARenaissance&collections=Perseus%3Acollection%3ARichTimes&collections=Perseus%3Acollection%3APDILL&all_words=${encodeURIComponent(
         word
     )}&phrase=&any_words=&exclude_words=&search=Search`;
 };
