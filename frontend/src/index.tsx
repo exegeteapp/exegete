dca800c78b5038a4bf1879f181329a0227e4176a
Grahame Bowland <grahame@bowland.au>
2025-10-24 01:36:42 +0800

    security updates to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -16,7 +16,6 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 // address CSP issues with react-fontawesome
 config.autoAddCss = false;
@@ -32,7 +31,3 @@ root.render(
     </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
